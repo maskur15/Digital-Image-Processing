7f897067ea87772332f32eea8cf62a7f7b2532fe
maskur15 <54944422+maskur15@users.noreply.github.com>
2023-02-09 15:14:15 +0600

    Update and rename numpyArrayToimage.ipynb to numpyArrayToimage.py

--- numpyArrayToimage.py
+++ numpyArrayToimage.py
@@ -1,7 +1,8 @@
 from PIL import Image
 import numpy as np
 #goldFish is the numpy array 
-numpy_image=goldFish*255.0 #if the numpy array is scaled in between 0 to 1 multiply by 255.0 to get the color image 
+numpy_image=goldFish*255.0
+#if the numpy array is scaled in between 0 to 1 multiply by 255.0 to get the color image 
 PIL_image = Image.fromarray(np.uint8(numpy_image)).convert('RGB')
 PIL_image
 #also the following code can work 
